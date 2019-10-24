4580c3b18200929e01a5b7e1988c8b9f7eb08322
Karol Siedlik <chillout550@gmail.com>
2019-10-24 19:48:56 +0200

    poprawka

--- Wallet.js
+++ Wallet.js
@@ -1,34 +1,27 @@
 class Wallet {
- constructor(money) {
-  let _money = money;
-  //pobieranie aktualnej zawartości portfela
-  this.getWalletValue = () => _money;
+  constructor(money) {
+    let _money = money;
 
-  //Sprawdzanie czy użytkonik ma odpowiednią ilość środków
+    this.getWalletValue = () => _money;
 
-  this.checkCanPlay = value => {
-   if (_money >= value) return true;
-   return false;
+    this.checkCanPlay = value => {
+      if (_money >= value) return true;
+      return false;
+    };
+
+    this.changeWallet = (value, type = "+") => {
+      if (typeof value === "number" && !isNaN(value)) {
+        if (type === "+") {
+          return (_money += value);
+        } else if (type === "-") {
+          return (_money -= value);
+        } else {
+          throw new Error("nieprawidłowy typ działania");
+        }
+      } else {
+        console.log(typeof value);
+        throw new Error("nieprawdidłowa liczba");
+      }
+    };
   }
-
-  this.changeWallet = (value, type = "+") => {
-   if (typeof value === "number" && !isNaN(value)) {
-    if (type === "+") {
-     return _money += value;
-    } else if (type === "-") {
-     return _money -= value;
-    } else {
-     throw new Error("nieprawidłowy typ działania")
-    }
-
-   } else {
-    console.log(typeof value);
-    throw new Error("nieprawdidłowa liczba")
-   }
-  }
-
- }
-
 }
-
-// const wallet = new Wallet(200)
